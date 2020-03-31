f8e1ea26c917b4a65cefc89d4621205c90df3858
Bojan Cakić <cakicbojan@gmail.com>
2020-03-31 17:46:27 +0200

    Starting project structure

---
+++ backend/src/test/java/siit/isamrs2020/backend/BackendApiApplicationTests.java
@@ -0,0 +1,13 @@
+package siit.isamrs2020.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
